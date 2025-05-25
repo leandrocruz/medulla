0f1af3e0f5bcc4312b7c926483bd5869ec108816
Leandro Cruz <leandro.saad@gmail.com>
2025-05-25 16:23:02 -0300

    Minor changes

--- src/main/scala/medulla/shared.scala
+++ src/main/scala/medulla/shared.scala
@@ -3,6 +3,7 @@ package medulla
 object Pages {
   sealed trait Page
   case object HomePage                 extends Page
+  case object TestPage                 extends Page
   case class  UnknownPage(str: String) extends Page
 }
 
@@ -11,9 +12,6 @@ object MedullaRouter {
   import com.raquo.laminar.api.L
   import com.raquo.laminar.api.L.*
   import com.raquo.waypoint.*
-  import org.scalajs.dom
-  import org.scalajs.dom.console
-
   import medulla.Pages.*
   import com.raquo.waypoint.Router
   import io.circe.*
@@ -24,17 +22,22 @@ object MedullaRouter {
   private def title        (page: Page)   : String  = "Console Medulla"
   private def encodePage   (page: Page)   : String  = page.asJson.spaces2SortKeys
   private def decodePage   (data: String) : Page    = decode[Page](data).getOrElse(UnknownPage(data))
-  private def unknownRoute (str: String)  : Page    = HomePage
+  private def unknownRoute (str: String)  : Page    = UnknownPage(str)
 
   object HomePageView {
-    def route = Route.static(HomePage, root)
+    def route = Route.static(HomePage, root / "home")
+  }
+
+  object TestPageView {
+    def route = Route.static(TestPage, root / "test")
   }
 
   val routes = List(
-    HomePageView.route
+    TestPageView.route,
+    HomePageView.route,
   )
 
-  private val router = Router[Page](routes, encodePage, decodePage, title)
+  private val router = Router[Page](routes, encodePage, decodePage, title, unknownRoute)
 
   def page: StrictSignal[Page] = router.currentPageSignal
 }
@@ -77,10 +80,10 @@ object login {
 
 object render {
 
+  import medulla.Pages.*
   import org.scalajs.dom.*
   import com.raquo.laminar.api.L.*
   import com.raquo.waypoint.SplitRender
-  import medulla.Pages.Page
 
   trait AppRender {
     def whenLoggedIn(user: UserToken) : Signal[HtmlElement]
@@ -91,34 +94,15 @@ object render {
 
     override def whenLoggedOut = Signal.fromValue(div("Logged Out!"))
 
-    override def whenLoggedIn(user: UserToken) = {
-      val body: SplitRender[Page, HtmlElement] = SplitRender[Page, HtmlElement](MedullaRouter.page).collect {
-        case p => div(
-          b(s"Hello ${user.name}!"),
-          br(),
-          small(s"(page: $p)")
-        )
-      }
-      body.signal
-    }
-  }
-}
-
-object tmp {
-  trait A {
-    def callB(): String
-  }
-
-  trait B {
-    def str: String
-  }
-
-  class AImpl(b: B) extends A {
-    override def callB() = b.str
-  }
-
-  class BImpl extends B {
-    override def str = "I'm bbb"
+    override def whenLoggedIn(user: UserToken) = SplitRender[Page, HtmlElement](MedullaRouter.page).collect {
+      case TestPage       => div("test")
+      case HomePage       => div("home")
+      case UnknownPage(p) => div(
+        b(s"Hello ${user.name}!"),
+        br(),
+        small(s"(page: $p)")
+      )
+    }.signal
   }
 }
 
@@ -145,7 +129,6 @@ object config {
 
 object cookie {
 
-  import org.scalajs.dom.document
   import scala.scalajs.js.URIUtils.decodeURIComponent
 
   case class Cookie(
